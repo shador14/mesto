a434715800fe2a478b767b1044d4953b9fd8d858
Vladimir Nazarov <v.nazarov@inbox.ru>
2022-02-11 14:23:55 +0400

    js open popup and like

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,32 @@
-console.log('Привет!');
+const elementLike = document.querySelector('.element__like');
+const toggleElementLike = function() {
+  elementLike.classList.toggle('element__like_active');
+};
+
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.profile__button-edit');
+
+const openPopup = function() {
+  popupElement.classList.add('popup_is-opened');
+};
+
+const closePopup = function() {
+  popupElement.classList.remove('popup_is-opened');
+};
+
+const closePopupByClickOnOverlay = function(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+
+  closePopup()
+};
+
+
+//Регистрируем обработчики событий по клику
+elementLike.addEventListener('click', toggleElementLike);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+
