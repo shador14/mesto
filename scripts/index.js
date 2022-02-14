ef993658a3ddd0ac925acf5fcbee07d4c16a9357
Vladimir Nazarov <v.nazarov@inbox.ru>
2022-02-14 11:04:31 +0400

    fix java

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,31 @@
-//Лайки
-const elementLike = document.querySelector('.element__like');
-const toggleElementLike = function() {
-  elementLike.classList.toggle('element__like_active');
-};
-
-//Открытие и закрытие поп-апа
+//Выборка DOM эелементов
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__button-edit');
 
+const submitPopup = document.querySelector('.popup__submite');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+const inputName = popupElement.querySelector('.popup__el_type_name');
+const inputJob = popupElement.querySelector('.popup__el_type_job');
+
+//Функции
 const openPopup = function() {
   popupElement.classList.add('popup_is-opened');
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
 };
+
 const closePopup = function() {
   popupElement.classList.remove('popup_is-opened');
 };
+
 const closePopupByClickOnOverlay = function(event) {
   if (event.target !== event.currentTarget) {
     return;
   }
   closePopup()
 };
-
-//Связь формы
-const submitPopup = document.querySelector('.popup__submite');
-const profileName = document.querySelector('.profile__title');
-const profileJob = document.querySelector('.profile__subtitle');
-const inputName = popupElement.querySelector('.popup__el_name');
-const inputJob = popupElement.querySelector('.popup__el_job');
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -37,7 +35,6 @@ function formSubmitHandler (evt) {
 };
 
 //Регистрируем обработчики событий по клику
-elementLike.addEventListener('click', toggleElementLike);
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
